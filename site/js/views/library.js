6b29af52f00a4cbc0f1c2b933d54b2d4106b94f4
Marc Cyr <marcacyr@gmail.com>
2014-04-07 16:53:17 -0700

    finish up with the app so that it is using REST API and all functionality is there

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -7,25 +7,39 @@ app.LibraryView = Backbone.View.extend({
     'click #add': 'addBook'
   },
 
-  addBook: function(e) {
+  addBook: function( e ) {
     e.preventDefault();
 
     var formData = {};
 
-    $('#addBook div').children('input').each(function(i, el) {
-      if ($(el).val() != '') {
-        formData[el.id] = $(el).val();
-      }
+    $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
+        if( $( el ).val() != '' )
+        {
+            if( el.id === 'keywords' ) {
+                formData[ el.id ] = [];
+                _.each( $( el ).val().split( ' ' ), function( keyword ) {
+                    formData[ el.id ].push({ 'keyword': keyword });
+                });
+            } else if( el.id === 'releaseDate' ) {
+                formData[ el.id ] = $( '#releaseDate' ).datepicker( 'getDate' ).getTime();
+            } else {
+                formData[ el.id ] = $( el ).val();
+            }
+        }
+        // Clear input field value
+        $( el ).val('');
     });
 
-    this.collection.add(new app.Book(formData));
-  },
+    this.collection.create( formData );
+},
 
   initialize: function(initialBooks) {
-    this.collection = new app.Library(initialBooks);
+    this.collection = new app.Library();
+    this.collection.fetch({reset: true});
     this.render();
 
     this.listenTo(this.collection, 'add', this.renderBook);
+    this.listenTo(this.collection, 'reset', this.render);
   },
 
   render: function() {
